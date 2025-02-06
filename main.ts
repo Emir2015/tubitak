3a03f7ab137f905dc37a3fe2a4a766ad5025d7c2
Emir2015 <emir15151515155@gmail.com>
2025-02-06 18:51:18 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+function dönme () {
+    servos.P0.run(10)
+    basic.pause(100)
+    servos.P0.stop()
+}
+let değişken = 0
+for (let index = 0; index < 24; index++) {
+    değişken = 10
+    basic.pause(5000)
+}
+basic.forever(function () {
+    if (değişken == 10) {
+        dönme()
+    } else if (değişken == 20) {
+        dönme()
+    } else if (değişken == 30) {
+        dönme()
+    } else if (değişken == 40) {
+        dönme()
+    } else if (değişken == 50) {
+        dönme()
+    } else if (değişken == 60) {
+        dönme()
+    } else if (değişken == 70) {
+        dönme()
+    } else if (değişken == 80) {
+        dönme()
+    } else if (değişken == 90) {
+        dönme()
+    } else if (değişken == 90) {
+        dönme()
+    } else if (değişken == 90) {
+        dönme()
+    } else if (değişken == 90) {
+        dönme()
+    } else if (değişken == 100) {
+        dönme()
+    } else if (değişken == 110) {
+        dönme()
+    } else if (değişken == 120) {
+        dönme()
+    } else if (değişken == 130) {
+        dönme()
+    } else if (değişken == 140) {
+        dönme()
+    } else if (değişken == 150) {
+        dönme()
+    } else if (değişken == 160) {
+        dönme()
+    } else if (değişken == 170) {
+        dönme()
+    } else if (değişken == 180) {
+        dönme()
+    } else if (değişken == 190) {
+        dönme()
+    } else if (değişken == 200) {
+        dönme()
+    } else if (değişken == 210) {
+        dönme()
+    } else if (değişken == 220) {
+        dönme()
+    } else if (değişken == 230) {
+        dönme()
+    } else {
+        dönme()
+    }
+})
